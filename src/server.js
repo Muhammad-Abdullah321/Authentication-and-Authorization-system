e95db4dbd696ddeea0c502f11a7cbae651152915
Muhammad Abdullah <abdullah03362@gmail.com>
2026-09-15 22:21:13 +0500

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 import app from "./app.js";
-import { connectDB } from "./config/config.js";
+import { connectDB ,PORT} from "./config/config.js";
 import googlePassport from "./config/google.pass.config.js";
 import githubPassport from "./config/github.passport.js";
 
-const port = 3000;
 
 app.use(googlePassport.initialize());
 app.use(githubPassport.initialize());
 const startServer = async () => {
     try {
         await connectDB();
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
         console.error("Failed to start server:", error.message);
